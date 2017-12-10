e41fdcad70f44c31c4d54d652c9a0b4ec86dc3d7
Craig Mathieson <craigmathieson@uvic.ca>
2017-12-09 18:36:31 -0800

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Craig Mathieson on 2017-12-09.
+//  Copyright © 2017 Craig Mathieson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
