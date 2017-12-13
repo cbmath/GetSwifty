428388254b1c567bfc9f1faa39b9b3b4e2508e02
Craig Mathieson <craigsskiingyo@gmail.com>
2017-12-13 10:41:19 -0800

    added subtraction functionality

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -11,19 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var disLabel: UILabel!
-    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     var buttonCount = 0
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func changeText(_ sender: Any) {
         
-        buttonCount = buttonCount + 1
+        buttonCount += 1
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            disLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            disLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
+        
         if buttonCount == 10 {
             view.backgroundColor = UIColor.red
-            disLabel.text = "eskeeeiiittt"
+            disLabel.text = "issa red calculator"
             //this changes text and background
         }
-        print(buttonCount)
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
